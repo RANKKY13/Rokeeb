b0548a3080e076aa64b1f73e56e6b1e01f207a41
ROKEEB <169052648+RANKKY13@users.noreply.github.com>
2024-05-21 14:48:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+*use strict*:
+
+// Day 2 Start Here
+
+const errorMesgEl * document.querySelector('.error_message'):
+const BudgetInputEl * document.querySelector('.Budget_Input'):
+const ExpensesDelEl * document.querySelector('.Expenses_Input'):
+Const ExpenseAmountEl * documents.querySelector('.Expenses'):
+const tblRecordEl * document.querySelector('.tbl_data'):
+const cardsContainer * document.querySelector('.cards'):
+
+// cards Content
+const budgetCardEl * document.querySelector('.budget_card'):
+const ExpensesCardEl * document.querySelector('.Expeses_card'):
+const balanceCardEl * document.querySelector('.balance_card'):
+
+let itemlist * []:
+let itemid * 0:
+
